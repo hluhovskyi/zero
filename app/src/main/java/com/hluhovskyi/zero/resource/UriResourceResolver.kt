4d47e6f5cdf1055c243249241fc9817ca66fdfd7
Artem Hluhovskyi <hluhovskyi@gmail.com>
2022-03-30 17:51:12 +0300

    Clean up

--- app/src/main/java/com/hluhovskyi/zero/resource/UriResourceResolver.kt
+++ app/src/main/java/com/hluhovskyi/zero/resource/UriResourceResolver.kt
@@ -8,18 +8,3 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.onStart
 import java.io.FileInputStream
 
-internal class UriResourceResolver(
-    private val context: Context,
-) : ResourceResolver by resourceResolverOf<UriRequest, UriResult>({ request ->
-    flow<ResourceStatus<UriResult>> {
-        val uri = request.uri
-        when {
-            uri !is Uri.NonEmpty -> {}
-            request.uri.isFile -> emit(ResourceStatus.Result(UriResult(FileInputStream(uri.value))))
-            request.uri.isAndroid -> emit(ResourceStatus.Result(UriResult(
-                context.contentResolver.openInputStream(android.net.Uri.parse(uri.value))!!)
-            ))
-        }
-    }
-        .onStart { emit(ResourceStatus.Idle()) }
-})
---
+++ zero-core/src/main/java/com/hluhovskyi/zero/resource/ResourceResolverComponent.kt
@@ -0,0 +1,54 @@
+package com.hluhovskyi.zero.resource
+
+import android.content.Context
+import com.hluhovskyi.zero.common.Buildable
+import dagger.Provides
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.SOURCE)
+private annotation class ResourceResolverScope
+
+@ResourceResolverScope
+@dagger.Component(
+    modules = [ResourceResolverComponent.Module::class],
+    dependencies = [ResourceResolverComponent.Dependencies::class]
+)
+abstract class ResourceResolverComponent {
+
+    abstract val resourceResolver: ResourceResolver
+
+    interface Dependencies {
+        val context: Context
+    }
+
+    companion object {
+
+        fun builder(dependencies: Dependencies): Builder = DaggerResourceResolverComponent.builder()
+            .dependencies(dependencies)
+    }
+
+    @dagger.Component.Builder
+    interface Builder : Buildable<ResourceResolverComponent> {
+
+        fun dependencies(dependencies: Dependencies): Builder
+    }
+
+    @dagger.Module
+    object Module {
+
+        @Provides
+        @ResourceResolverScope
+        internal fun uriResourceResolver(
+            context: Context
+        ): UriResourceResolver = UriResourceResolver(context)
+
+        @Provides
+        @ResourceResolverScope
+        internal fun compositeResourceResolver(
+            uriResourceResolver: UriResourceResolver
+        ): ResourceResolver = CompositeResourceResolver(
+            mapOf(UriRequest::class to uriResourceResolver)
+        )
+    }
+}
