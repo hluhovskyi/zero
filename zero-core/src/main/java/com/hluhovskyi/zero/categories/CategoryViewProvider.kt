837d62ee02f31ee1f7a20199241f322c220222c9
Artem Hluhovskyi <hluhovskyi@gmail.com>
2026-03-29 22:18:48 -0700

    refactor: use CategoryIconView in categories list

--- zero-core/src/main/java/com/hluhovskyi/zero/categories/CategoryViewProvider.kt
+++ zero-core/src/main/java/com/hluhovskyi/zero/categories/CategoryViewProvider.kt
@@ -1,18 +1,14 @@
 package com.hluhovskyi.zero.categories
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -23,6 +19,7 @@ import androidx.compose.ui.unit.dp
 import com.hluhovskyi.zero.ImageLoader
 import com.hluhovskyi.zero.common.ViewProvider
 import com.hluhovskyi.zero.common.toCompose
+import com.hluhovskyi.zero.ui.CategoryIconView
 
 internal class CategoryViewProvider(
     private val viewModel: CategoryViewModel,
@@ -55,12 +52,7 @@ private fun CategoryView(
                     .padding(horizontal = 16.dp, vertical = 12.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Box(
-                    modifier = Modifier
-                        .background(color = category.color.toCompose(), shape = CircleShape)
-                        .size(40.dp),
-                    contentAlignment = Alignment.Center,
-                ) {
+                CategoryIconView(color = category.color.toCompose()) {
                     imageLoader.View(
                         image = category.icon,
                         modifier = Modifier
