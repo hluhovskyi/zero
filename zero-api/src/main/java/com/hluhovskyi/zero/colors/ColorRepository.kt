421f7bae88170daa74830320245b48b9b39bc5f2
Artem Hluhovskyi <hluhovskyi@gmail.com>
2026-03-29 23:19:46 -0700

    feat: add ColorScheme and schemeFor() to ColorRepository

--- zero-api/src/main/java/com/hluhovskyi/zero/colors/ColorRepository.kt
+++ zero-api/src/main/java/com/hluhovskyi/zero/colors/ColorRepository.kt
@@ -7,6 +7,8 @@ interface ColorRepository {
 
     fun <T> query(criteria: Criteria<T>): Flow<T>
 
+    fun schemeFor(colorId: Id.Known): ColorScheme
+
     sealed interface Criteria<T> {
         class All : Criteria<List<Color>>
         data class ById(val id: Id.Known) : Criteria<Color>
