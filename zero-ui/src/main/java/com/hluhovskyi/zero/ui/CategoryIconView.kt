33c0580f8cfbcefe1fa612dc648497bed3610c4b
Artem Hluhovskyi <hluhovskyi@gmail.com>
2026-03-29 22:16:19 -0700

    feat: add reusable CategoryIconView composable

---
+++ zero-ui/src/main/java/com/hluhovskyi/zero/ui/CategoryIconView.kt
@@ -0,0 +1,32 @@
+package com.hluhovskyi.zero.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CategoryIconView(
+    color: Color,
+    size: Dp = 40.dp,
+    contentPadding: Dp = 8.dp,
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .background(color, shape = RoundedCornerShape(percent = 30))
+            .size(size)
+            .padding(contentPadding),
+        contentAlignment = Alignment.Center,
+        content = content,
+    )
+}
