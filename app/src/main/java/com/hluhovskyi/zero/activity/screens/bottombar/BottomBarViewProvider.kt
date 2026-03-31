8a80ed2480448d804b00a21729d4beb32301d331
Artem Hluhovskyi <hluhovskyi@gmail.com>
2026-03-31 00:13:26 -0700

    View imports

--- app/src/main/java/com/hluhovskyi/zero/activity/screens/bottombar/BottomBarViewProvider.kt
+++ app/src/main/java/com/hluhovskyi/zero/activity/screens/bottombar/BottomBarViewProvider.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.hluhovskyi.zero.ImageLoader
+import com.hluhovskyi.zero.View
 import com.hluhovskyi.zero.common.ViewProvider
 
 internal class BottomBarViewProvider(
