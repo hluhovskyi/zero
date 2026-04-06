4bc3b2fb761d8ab71e79760762950911f596cd75
Artem Hluhovskyi <hluhovskyi@gmail.com>
2026-04-05 23:47:40 -0700

    feat: scaffold lint-rules module with ZeroIssueRegistry

---
+++ lint-rules/src/main/kotlin/com/hluhovskyi/zero/lint/ZeroIssueRegistry.kt
@@ -0,0 +1,15 @@
+package com.hluhovskyi.zero.lint
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.client.api.Vendor
+import com.android.tools.lint.detector.api.CURRENT_API
+import com.android.tools.lint.detector.api.Issue
+
+class ZeroIssueRegistry : IssueRegistry() {
+
+    override val issues: List<Issue> = emptyList()
+
+    override val api: Int = CURRENT_API
+
+    override val vendor: Vendor = Vendor(vendorName = "Zero")
+}
