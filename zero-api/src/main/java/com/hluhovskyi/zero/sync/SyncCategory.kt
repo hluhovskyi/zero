6ce79400e79b01e461d4d0dfd50eba563ac1dca2
Artem Hluhovskyi <hluhovskyi@gmail.com>
2026-04-12 14:34:45 -0700

    feat(zero-api): add SyncCategory, SyncAccount, SyncTransaction DTOs

---
+++ zero-api/src/main/java/com/hluhovskyi/zero/sync/SyncCategory.kt
@@ -0,0 +1,18 @@
+package com.hluhovskyi.zero.sync
+
+import com.hluhovskyi.zero.common.Id
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SyncCategory(
+    @SerialName("id") @Serializable(with = IdKnownSerializer::class) override val id: Id.Known,
+    @SerialName("name") val name: String,
+    @SerialName("iconId") val iconId: String?,
+    @SerialName("colorId") val colorId: String?,
+    @SerialName("parentCategoryId") val parentCategoryId: String?,
+    @SerialName("creationDateTime") val creationDateTime: LocalDateTime,
+    @SerialName("updatedDateTime") override val updatedDateTime: LocalDateTime,
+    @SerialName("deletedAt") override val deletedAt: LocalDateTime?,
+) : SyncEntity
